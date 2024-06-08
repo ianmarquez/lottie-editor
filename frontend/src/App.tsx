e6b0c41851a1773ba8558ab951ffbe92f7ca7a73
Ian Marquez <regilmarquez123@gmail.com>
2024-06-08 20:03:20 +0800

    Update: Added front end scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="font-semibold text-2xl text-red-100">Test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
